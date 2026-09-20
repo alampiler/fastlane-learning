aa2b27fce9315e7da6674951066e480ef979e50d
Vadym Vozniuk <alampil33149@gmail.com>
2026-09-20 21:38:27 +0300

    Remove default app boilerplate

--- App.tsx
+++ App.tsx
@@ -5,11 +5,11 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
+// import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
+  // useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import Config from 'react-native-config';
 
