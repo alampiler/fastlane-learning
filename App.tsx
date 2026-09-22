890074f20a7a238f7ba94fdd37cda5a1fc44f8a6
Vadym Vozniuk <alampil33149@gmail.com>
2026-09-22 14:44:40 +0300

    Add maestro and button

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@
  * @format
  */
 
-// import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+//import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, Button } from 'react-native';
+import { useState } from 'react';
 import {
   SafeAreaProvider,
   // useSafeAreaInsets,
@@ -27,24 +28,27 @@ function App() {
 
 
 function AppContent() {
-  // const safeAreaInsets = useSafeAreaInsets();
+    // const safeAreaInsets = useSafeAreaInsets();
+    const [pressed, setPressed] = useState(false);
 
-  return (
-    <View style={styles.container}>
-      {/* <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      /> */}
-      <Text style={{ textAlign: 'center', marginBottom: 20 }}>
-        Edit <Text style={{ fontWeight: 'bold' }}>App.tsx</Text> to change this
-        screen and then come back to see your edits. 2
-      </Text>
-      <Text>
-        Config.API_URL: {Config.API_URL}
-      </Text>
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        {/* <NewAppScreen
+          templateFileName="App.tsx"
+          safeAreaInsets={safeAreaInsets}
+        /> */}
+        <Text style={{ textAlign: 'center', marginBottom: 20 }}>
+          Edit <Text style={{ fontWeight: 'bold' }}>App.tsx</Text> to change this
+          screen and then come back to see your edits. 2
+        </Text>
+        <Text>
+          Config.API_URL: {Config.API_URL}
+        </Text>
+        <Button title="Press me" onPress={() => setPressed(true)} />
+        {pressed && <Text>Button pressed!</Text>}
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
